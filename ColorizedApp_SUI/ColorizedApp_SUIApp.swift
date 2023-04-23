c36c1209d1441d876210e2f7cb5a21ef9eac5198
@Annabel_ <hydrant94@gmail.com>
2023-04-23 23:20:35 +0400

    Initial Commit

---
+++ ColorizedApp_SUI/ColorizedApp_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SUIApp.swift
+//  ColorizedApp_SUI
+//
+//  Created by Аня Беликова on 23.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
